cfa66cb463dcbf6d2d6c796baba89dbdb8464816
samisalami <samgus05@gmail.com>
2026-01-20 12:45:40 +0100

    Expand framebuffer

--- src/engine/core/buffers/framebuffer.c
+++ src/engine/core/buffers/framebuffer.c
@@ -1,8 +1,25 @@
 #include "framebuffer.h"
 #include <glad/glad.h>
+#include <stdio.h>
 
 void framebuffer_init(framebuffer* fbo) {
-	
+	glGenFramebuffers(1, &fbo->handle);
+
+	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
+		fprintf(stderr, "Framebuffer is not complete\n");
+		return;
+	}
 }
 
-void framebuffer_deinit(framebuffer* fbo);
+// Default usage is GL_FRAMEBUFFER
+void framebuffer_use(framebuffer* fbo, GLenum usage) {
+	glBindFramebuffer(usage, fbo->handle);
+}
+
+void framebuffer_unbind(framebuffer* fbo) {
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
+
+void framebuffer_deinit(framebuffer* fbo) {
+	glDeleteFramebuffers(1, &fbo->handle);
+}
